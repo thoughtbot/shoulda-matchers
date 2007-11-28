2d7a0264e49e753e88acd2ee79b76fa5347e980a
tsaleh <tsaleh@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2007-11-28 23:05:58 +0000

    comments
    git-svn-id: https://svn.thoughtbot.com/plugins/shoulda/trunk@265 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -6,6 +6,9 @@ $VERBOSE = old_verbose
 require File.join(File.dirname(__FILE__), 'boot')
  
 Rails::Initializer.run do |config|
+  # Someday, I'm going to find a way of getting rid of that symlink...
+  # config.plugin_paths = ['../../../']
+  # config.plugins = [:shoulda]
   config.log_level = :debug
   config.cache_classes = false
   config.whiny_nils = true
