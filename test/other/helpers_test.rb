789a6884f8a4b9e8c5edf0e4465ed7c83b0d9ac4
Ryan McGeary <ryanongit@mcgeary.org>
2008-07-09 09:53:50 -0400

    Added new should_change and should_not_change macros; akin to Active Support's assert_difference.

--- test/other/helpers_test.rb
+++ test/other/helpers_test.rb
@@ -77,4 +77,35 @@ class HelpersTest < Test::Unit::TestCase # :nodoc:
       end
     end
   end
+  
+  context "an array of numbers" do
+    setup do
+      @a = [1, 2, 3]
+    end
+
+    context "after adding another value" do
+      setup do
+        @a.push(4)
+      end
+
+      should_change "@a.length", :by => 1
+      should_change "@a.length", :from => 3
+      should_change "@a.length", :to => 4
+      should_change "@a[0]", :by => 0
+      should_not_change "@a[0]"
+    end
+
+    context "after replacing it with an array of strings" do
+      setup do
+        @a = %w(a b c d e f)
+      end
+
+      should_change "@a.length", :by => 3
+      should_change "@a.length", :from => 3, :to => 6, :by => 3
+      should_change "@a[0]"
+      should_change "@a[1]", :from => 2, :to => "b"
+      should_change "@a[2]", :from => 3
+      should_change "@a[3]", :to => "d"
+    end
+  end
 end
