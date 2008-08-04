56a3f432b274b1762b26ba9516762c4cdc27dd17
Tammer Saleh <tsaleh@thoughtbot.com>
2008-08-04 16:17:32 -0400

    Added shoulda_macros directory support as per http://thoughtbot.lighthouseapp.com/projects/5807/tickets/62

--- init.rb
+++ init.rb
@@ -1,3 +1,15 @@
 require 'rubygems'
 require 'active_support'
 require 'shoulda'
+
+if defined?(RAILS_ROOT)
+  # load in the 3rd party macros from vendorized plugins and gems
+  Dir[File.join(RAILS_ROOT, "vendor", "{plugin,gem}", "*", "shoulda_macros", "*.rb")].each do |macro_file_path|
+    require macro_file_path
+  end
+
+  # load in the local application specific macros
+  Dir[File.join(RAILS_ROOT, "test", "shoulda_macros", "*.rb")].each do |macro_file_path|
+    require macro_file_path
+  end
+end
