0040ccda1bb78e8e259f49f7896eb2d94f988263
Ryan McGeary <ryanongit@mcgeary.org>
2008-08-31 12:46:38 -0400

    Testing fix for incorrectly specified :source option on a has_many :through

--- test/rails_root/app/models/post.rb
+++ test/rails_root/app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :owner, :foreign_key => :user_id, :class_name => 'User'
   has_many :taggings
   has_many :tags, :through => :taggings
-  has_many :through_tags, :through => :taggings, :source => :tags
-  
+  has_many :through_tags, :through => :taggings, :source => :tag
+
   validates_uniqueness_of :title
   validates_presence_of :title
   validates_presence_of :body, :message => 'Seriously...  wtf'
