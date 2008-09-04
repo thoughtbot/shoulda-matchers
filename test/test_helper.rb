cfd8c7e305526813c77c176d9b36e561fa0ccb98
Ryan McGeary <ryanongit@mcgeary.org>
2008-09-03 20:37:28 -0400

    Added a `should_fail` macro to help test failure scenarios in the core shoulda tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,15 +5,15 @@ ENV['RAILS_ENV'] = 'sqlite3'
 rails_root = File.dirname(__FILE__) + '/rails_root'
 
 require "#{rails_root}/config/environment.rb"
- 
+
 # Load the testing framework
 require 'test_help'
 silence_warnings { RAILS_ENV = ENV['RAILS_ENV'] }
- 
+
 # Run the migrations
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Migrator.migrate("#{RAILS_ROOT}/db/migrate")
- 
+
 # Setup the fixtures path
 Test::Unit::TestCase.fixture_path = File.join(File.dirname(__FILE__), "fixtures")
 
@@ -25,7 +25,9 @@ class Test::Unit::TestCase #:nodoc:
       Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names)
     end
   end
- 
+
   self.use_transactional_fixtures = false
   self.use_instantiated_fixtures  = false
 end
+
+require 'test/fail_macros'
