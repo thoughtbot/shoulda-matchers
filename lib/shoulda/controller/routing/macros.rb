56112d9916fccd526ef447040fd95fed63530592
Josh Nichols <josh@technicalpickles.com>
2008-09-12 16:03:03 -0400

    Added macros.

---
+++ lib/shoulda/controller/routing/macros.rb
@@ -0,0 +1,26 @@
+module ThoughtBot
+  module Shoulda
+    module RailsRouting
+      module Macros
+        def should_route(method, path, options)
+          unless options[:controller]
+            options[:controller] = self.class.name.gsub(/ControllerTest$/, '').tableize
+          end
+          options[:controller] = options[:controller].to_s
+          options[:action] = options[:action].to_s
+
+          populated_path = path.dup
+          options.each do |key, value|
+            populated_path.gsub!(key.inspect, value.to_s)
+          end
+
+          should_name = "route #{method.to_s.upcase} #{populated_path} to/from #{options.inspect}"
+
+          should should_name do
+            assert_routing({:method => method, :path => populated_path}, options)
+          end
+        end
+      end
+    end
+  end
+end
