d20c85e112e3a25dd8792ae85932631878770a9c
Josh Nichols <josh@technicalpickles.com>
2008-09-12 13:22:59 -0400

    Tweaked init.rb to only load activerecord/actionmailer/controller stuff if the appropriate classes are defined.

--- init.rb
+++ init.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 require 'active_support'
 require 'shoulda'
-require 'shoulda/active_record'
-require 'shoulda/controller'
-require 'shoulda/action_mailer'
+
+require 'shoulda/active_record' if defined? ActiveRecord::Base
+require 'shoulda/controller'    if defined? ActionController::Base
+require 'shoulda/action_mailer' if defined? ActionMailer::Base
 
 if defined?(RAILS_ROOT)
   # load in the 3rd party macros from vendorized plugins and gems
