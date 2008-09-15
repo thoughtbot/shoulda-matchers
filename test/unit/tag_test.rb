611f5f4dac6844daa19a1d64b7120f22de94a8b7
Ryan McGeary <ryanongit@mcgeary.org>
2008-09-15 08:55:03 -0400

    Added `should_fail` scenario for testing `should_protect_attributes`
      * Tests the failure case where the model uses attr_accessible instead of attr_protected

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -7,4 +7,8 @@ class TagTest < Test::Unit::TestCase
   should_ensure_length_at_least :name, 2
 
   should_protect_attributes :secret
+
+  should_fail do
+    should_protect_attributes :name
+  end
 end
