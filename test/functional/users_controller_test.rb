9a43008886c316ad42c3bd1a21e2d7dc8aae4549
Tammer Saleh <tsaleh@thoughtbot.com>
2008-09-20 22:08:45 -0400

    Revert the removal of should_be_restful.
    This reverts commits 10303b28f38c76c83bf99037eb9b2cb05ffec737,
    f018d0844403ccdf4d89758966a2914b4d317c55 and
    66eaccf98ce4948f876f87fc0dbb51691eb8d7a7, which collectively removed
    should_be_restful.  We need to deprecate should_be_restful for a good while
    before we actually remove it from the codebase.
    
    This also reverts the gem version back to 2.0.0.  We'll see if github has a
    problem with that.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -14,30 +14,23 @@ class UsersControllerTest < Test::Unit::TestCase
     @user       = User.find(:first)
   end
 
-  context "on GET to #index" do
-    setup { get :index }
+  should_be_restful do |resource|
+    resource.identifier = :id
+    resource.klass      = User
+    resource.object     = :user
+    resource.parent     = []
+    resource.actions    = [:index, :show, :new, :edit, :update, :create, :destroy]
+    resource.formats    = [:html, :xml]
 
-    should_respond_with :success
-    should_render_with_layout 'users'
-    should_render_template :index
-    should_assign_to :users
-  end
-  
-  context "on GET to #index.xml" do
-    setup { get :index, :format => 'xml' }
-  
-    should_respond_with :success
-    should_respond_with_xml_for
-    should_assign_to :users
-  end
-  
-  context "on GET to #show" do
-    setup { get :show, :id => @user }
+    resource.create.params = { :name => "bob", :email => 'bob@bob.com', :age => 13, :ssn => "123456789"}
+    resource.update.params = { :name => "sue" }
 
-    should_respond_with :success
-    should_render_with_layout 'users'
-    should_render_template :show
-    should_assign_to :user
+    resource.create.redirect  = "user_url(@user)"
+    resource.update.redirect  = "user_url(@user)"
+    resource.destroy.redirect = "users_url"
+
+    resource.create.flash  = /created/i
+    resource.update.flash  = /updated/i
+    resource.destroy.flash = /removed/i
   end
-  
 end
