d96f5c315c66cdc34a86d1f1862d9feef187208f
Joe Ferris <joe.r.ferris@gmail.com>
2009-01-24 15:02:16 -0500

    Converted should_have_named_scope to use a matcher

--- lib/shoulda/active_record/macros.rb
+++ lib/shoulda/active_record/macros.rb
@@ -545,25 +545,11 @@ module Shoulda # :nodoc:
       #     scoped(:limit => c)
       #   end
       #
-      def should_have_named_scope(scope_call, *args)
+      def should_have_named_scope(scope_call, find_options = nil)
         klass = model_class
-        scope_opts = args.extract_options!
-        scope_call = scope_call.to_s
-
-        context scope_call do
-          setup do
-            @scope = eval("#{klass}.#{scope_call}")
-          end
-
-          should "return a scope object" do
-            assert_equal ::ActiveRecord::NamedScope::Scope, @scope.class
-          end
-
-          unless scope_opts.empty?
-            should "scope itself to #{scope_opts.inspect}" do
-              assert_equal scope_opts, @scope.proxy_options
-            end
-          end
+        matcher = have_named_scope(scope_call).finding(find_options)
+        should matcher.description do
+          assert_accepts matcher.in_context(self), klass.new
         end
       end
     end
---
+++ lib/shoulda/active_record/matchers/have_named_scope_matcher.rb
@@ -0,0 +1,85 @@
+module Shoulda # :nodoc:
+  module ActiveRecord # :nodoc:
+    module Matchers # :nodoc:
+
+      class HaveNamedScopeMatcher
+
+        def initialize(scope_call)
+          @scope_call = scope_call.to_s
+        end
+
+        def finding(finding)
+          @finding = finding
+          self
+        end
+
+        def in_context(context)
+          @context = context
+          self
+        end
+
+        def matches?(subject)
+          @subject = subject
+          call_succeeds? && returns_scope? && finds_correct_scope?
+        end
+
+        def failure_message
+          "Expected #{@missing_expectation}"
+        end
+
+        def negative_failure_message
+          "Didn't expect a named scope for #{@scope_call}"
+        end
+
+        def description
+          result = "have a named scope for #{@scope_call}"
+          result << " finding #{@finding.inspect}" unless @finding.nil?
+          result
+        end
+
+        private
+
+        def call_succeeds?
+          scope
+          true
+        rescue Exception => exception
+          @missing_expectation = "#{@subject.class.name} " <<
+            "to respond to #{@scope_call} " <<
+            "but raised error: #{exception.inspect}"
+          false
+        end
+
+        def scope
+          @scope ||= @context.instance_eval("#{@subject.class.name}.#{@scope_call}")
+        end
+
+        def returns_scope?
+          if ::ActiveRecord::NamedScope::Scope === scope
+            true
+          else
+            @missing_expectation = "#{@scope_call} to return a scope"
+            false
+          end
+        end
+
+        def finds_correct_scope?
+          return true if @finding.nil?
+          if @finding == scope.proxy_options
+            true
+          else
+            @missing_expectation = "#{@scope_call} to return results scoped to "
+            @missing_expectation << "#{@finding.inspect} but was scoped to "
+            @missing_expectation << scope.proxy_options.inspect
+            false
+          end
+        end
+
+      end
+
+      def have_named_scope(scope_call)
+        HaveNamedScopeMatcher.new(scope_call).in_context(self)
+      end
+
+    end
+  end
+end
