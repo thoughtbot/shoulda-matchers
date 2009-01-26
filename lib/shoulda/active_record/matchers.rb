e9e98b91489c1f01769cb7dcd0fd30d49ba01671
Joe Ferris <joe.r.ferris@gmail.com>
2009-01-26 18:34:04 -0500

    Added documentation in Shoulda::ActiveRecord::Matchers and mentioned Matchers in the README

--- lib/shoulda/active_record/matchers.rb
+++ lib/shoulda/active_record/matchers.rb
@@ -13,3 +13,30 @@ require 'shoulda/active_record/matchers/have_index_matcher'
 require 'shoulda/active_record/matchers/have_readonly_attribute_matcher'
 require 'shoulda/active_record/matchers/protect_attribute_matcher'
 require 'shoulda/active_record/matchers/have_named_scope_matcher'
+
+
+module Shoulda # :nodoc:
+  module ActiveRecord # :nodoc:
+    # = Matchers for your active record models
+    #
+    # These matchers will test most of the validations and associations for your
+    # ActiveRecord models.
+    #
+    #   describe User do
+    #     it { should require_attribute(:name) }
+    #     it { should require_attribute(:phone_number) }
+    #     %w(abcd 1234).each do |value|
+    #       it { should_not allow_value(value).for(:phone_number) }
+    #     end
+    #     it { should allow_value("(123) 456-7890").for(:phone_number) }
+    #     it { should protect_attribute(:password) }
+    #     it { should have_one(:profile) }
+    #     it { should have_many(:dogs) }
+    #     it { should have_many(:messes).through(:dogs) }
+    #     it { should belong_to(:lover) }
+    #   end
+    #
+    module Matchers
+    end
+  end
+end
