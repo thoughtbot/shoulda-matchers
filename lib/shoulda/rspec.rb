a37d4c57c5192ff427e0ac64e9a9a0dfccf5bb8b
Joe Ferris <joe.r.ferris@gmail.com>
2009-02-07 18:43:09 -0500

    Removed accidental Spec::Rails references from the docs

--- lib/shoulda/rspec.rb
+++ lib/shoulda/rspec.rb
@@ -1,8 +1,8 @@
 require 'shoulda/active_record/matchers'
 
-module Spec
-  module Rails
-    module Matchers
+module Spec # :nodoc:
+  module Rails # :nodoc:
+    module Matchers # :nodoc:
       include Shoulda::ActiveRecord::Matchers
     end
   end
