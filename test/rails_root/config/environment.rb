18c43a3434961296357f35c526437e9c1ee26b87
Matt Jankowski <mjankowski@thoughtbot.com>
2009-12-11 20:33:09 -0500

    allow to use newer installed rails gems

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -1,6 +1,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 old_verbose, $VERBOSE = $VERBOSE, nil
-RAILS_GEM_VERSION = '= 2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '>= 2.3.2' unless defined? RAILS_GEM_VERSION
 $VERBOSE = old_verbose
 
 require File.join(File.dirname(__FILE__), 'boot')
