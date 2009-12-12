6e4bb862e7a06c87a7331be2d828591103976010
Matt Jankowski <mjankowski@thoughtbot.com>
2009-12-11 20:36:08 -0500

    there is a bug in the I18n library bundled with newer rails versions which raises an error on :message options with dots in a row.  Removing from our test model does not affect our tests

--- test/rails_root/app/models/post.rb
+++ test/rails_root/app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ActiveRecord::Base
 
   validates_uniqueness_of :title
   validates_presence_of :title
-  validates_presence_of :body, :message => 'Seriously...  wtf'
+  validates_presence_of :body, :message => 'Seriously, wtf'
   validates_numericality_of :user_id
 end
