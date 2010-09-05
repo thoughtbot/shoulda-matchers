a08b3bf79acf30391830ab55b471f1c36c13180c
Frank Luithle <frank@luithle.net>
2010-09-05 17:33:32 +0200

    allow multiple values for AllowValueMatcher
    With the deprecation of "should_allow_values_for" there is currently no clean
    way to specify multiple allowed (or forbidden) values for an attribute in a
    single assertion. This can make test cases unnecessarily verbose.
    
    This patch gives AllowValueMatcher the ability to take multiple values. The new
    matcher is fully downward compatible (i.e. given a single argument it behaves
    identically to the old matcher).
    
    This matcher will stop checking for bad values when the first bad value is
    encountered.

--- test/matchers/active_record/allow_value_matcher_test.rb
+++ test/matchers/active_record/allow_value_matcher_test.rb
@@ -13,9 +13,17 @@ class AllowValueMatcherTest < ActiveSupport::TestCase # :nodoc:
     should "allow a good value" do
       assert_accepts allow_value("abcde").for(:attr), @model
     end
-    
+
+    should "allow several good values" do
+      assert_accepts allow_value("abcde", "deabc").for(:attr), @model
+    end
+
     should "not allow a bad value" do
       assert_rejects allow_value("xyz").for(:attr), @model
+    end
+
+    should "not allow several bad values" do
+      assert_rejects allow_value("xyz", "zyx", nil, []).for(:attr), @model
     end
   end
 
@@ -31,7 +39,7 @@ class AllowValueMatcherTest < ActiveSupport::TestCase # :nodoc:
       assert_accepts allow_value('abcde').for(:attr).with_message(/bad/),
                      @model
     end
-    
+
     should "not allow a bad value" do
       assert_rejects allow_value('xyz').for(:attr).with_message(/bad/),
                      @model
@@ -52,13 +60,35 @@ class AllowValueMatcherTest < ActiveSupport::TestCase # :nodoc:
     should "allow a good value" do
       assert_accepts allow_value("12345").for(:attr), @model
     end
-    
-    bad_values = [nil, "", "abc", "0", "50001", "123456"]
+
+    bad_values = [nil, "", "abc", "0", "50001", "123456", []]
+
     bad_values.each do |value|
       should "not allow a bad value (#{value.inspect})" do
         assert_rejects allow_value(value).for(:attr), @model
       end
     end
+
+    should "not allow bad values (#{bad_values.map {|v| v.inspect}.join(', ')})" do
+      assert_rejects allow_value(*bad_values).for(:attr), @model
+    end
   end
 
+  context "an AllowValueMatcher with multiple values" do
+    setup { @matcher = allow_value("foo", "bar").for(:baz) }
+
+    should "describe itself" do
+      expected = 'allow baz to be set to any of ["foo", "bar"]'
+      assert_equal expected, @matcher.description
+    end
+  end
+
+  context "an AllowValueMatcher with a single value" do
+    setup { @matcher = allow_value("foo").for(:baz) }
+
+    should "describe itself" do
+      expected = 'allow baz to be set to "foo"'
+      assert_equal expected, @matcher.description
+    end
+  end
 end
