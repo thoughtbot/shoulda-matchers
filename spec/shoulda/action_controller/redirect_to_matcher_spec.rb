b8343ef963fe691beea8c968d5d3dc969be63e37
Tristan Dunn <tristanzdunn@gmail.com>
2010-12-17 12:14:02 -0500

    Remove "should" from example descriptions in a couple of files.

--- spec/shoulda/action_controller/redirect_to_matcher_spec.rb
+++ spec/shoulda/action_controller/redirect_to_matcher_spec.rb
@@ -6,34 +6,34 @@ describe Shoulda::Matchers::ActionController::RedirectToMatcher do
       @controller = build_response { redirect_to '/some/url' }
     end
 
-    it "should accept redirecting to that url" do
+    it "accepts redirecting to that url" do
       @controller.should redirect_to('/some/url')
     end
 
-    it "should reject redirecting to a different url" do
+    it "rejects redirecting to a different url" do
       @controller.should_not redirect_to('/some/other/url')
     end
 
-    it "should accept redirecting to that url in a block" do
+    it "accepts redirecting to that url in a block" do
       @controller.should redirect_to('somewhere') { '/some/url' }
     end
 
-    it "should reject redirecting to a different url in a block" do
+    it "rejects redirecting to a different url in a block" do
       @controller.should_not redirect_to('somewhere else') { '/some/other/url' }
     end
   end
 
-  context "a  controller that doesn't redirect" do
+  context "a controller that doesn't redirect" do
     before do
       @controller = build_response { render :text => 'hello' }
     end
 
-    it "should reject redirecting to a url" do
+    it "rejects redirecting to a url" do
       @controller.should_not redirect_to('/some/url')
     end
   end
 
-  it "should use the correct description when provided a block" do
+  it "provides the correct description when provided a block" do
     matcher = redirect_to('somewhere else') { '/some/other/url' }
     matcher.description.should == 'redirect to somewhere else'
   end
