b39fba0bedb48db6e94b04301e463d8216000e91
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-31 16:30:31 -0500

    Switched to allowing a more canonical require statement
        require "shoulda/matchers"  # canonical
    
    -vs-
    
        require "shoulda-matchers"  # still allowed
    
    See comments of http://blog.segment7.net/2010/11/15/how-to-name-gems

--- lib/shoulda-matchers.rb
+++ lib/shoulda-matchers.rb
@@ -1,8 +1,1 @@
-require 'shoulda/matchers/version'
-
-if defined?(RSpec)
-  require 'shoulda/matchers/integrations/rspec'
-else
-  require 'shoulda/matchers/integrations/test_unit'
-end
-
+require 'shoulda/matchers'
---
+++ lib/shoulda/matchers.rb
@@ -0,0 +1,7 @@
+require 'shoulda/matchers/version'
+
+if defined?(RSpec)
+  require 'shoulda/matchers/integrations/rspec'
+else
+  require 'shoulda/matchers/integrations/test_unit'
+end
