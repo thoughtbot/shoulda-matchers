7f72f4a47800bbb9e995065ecf25e8ccf314226f
knapo <krzysztof.knapik@llp.pl>
2011-03-17 14:02:39 +0100

    Change the way of translating default_error_message by using :default I18n.translation options instead of throwing I18n::MissingTranslationData

--- lib/shoulda/matchers/active_record/helpers.rb
+++ lib/shoulda/matchers/active_record/helpers.rb
@@ -17,13 +17,8 @@ module Shoulda # :nodoc:
         #   default_error_message(:too_short, :count => 5)
         #   default_error_message(:too_long, :count => 60)
         def default_error_message(key, values = {})
-          if Object.const_defined?(:I18n) # Rails >= 2.2
-            result = I18n.translate("activerecord.errors.messages.#{key}", values)
-            if result =~ /^translation missing/
-              I18n.translate("errors.messages.#{key}", values)
-            else
-              result
-            end
+          if Object.const_defined?(:I18n) # Rails >= 2.2           
+            I18n.translate(:"activerecord.errors.messages.#{key}", {:default => :"errors.messages.#{key}"}.merge(values))
           else # Rails <= 2.1.x
             ::ActiveRecord::Errors.default_error_messages[key] % values[:count]
           end
