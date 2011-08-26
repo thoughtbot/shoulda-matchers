12455aeb67b49563b435f00a4e02e41ba92c24e3
Shaliko <shaliko@ezid.ru>
2011-08-26 18:52:32 +0400

    Edited lib/shoulda/matchers/action_controller/route_matcher.rb via GitHub

--- lib/shoulda/matchers/action_controller/route_matcher.rb
+++ lib/shoulda/matchers/action_controller/route_matcher.rb
@@ -16,7 +16,7 @@ module Shoulda # :nodoc:
       #   it { should route(:get, "/posts/new").to(:action => :new) }
       #   it { should route(:post, "/posts").to(:action => :create) }
       #   it { should route(:get, "/posts/1").to(:action => :show, :id => 1) }
-      #   it { should route(:get, "/posts/1").to(:action => :get, :id => 1) }
+      #   it { should route(:get, "/posts/1/edit").to(:action => :edit, :id => 1) }
       #   it { should route(:put, "/posts/1").to(:action => :update, :id => 1) }
       #   it { should route(:delete, "/posts/1").
       #                 to(:action => :destroy, :id => 1) }
