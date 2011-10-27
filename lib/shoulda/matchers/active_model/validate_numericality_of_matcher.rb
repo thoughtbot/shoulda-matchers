1ca20b2c39f16a1e97de951bf632a42ad0c6c955
Tomas Mattia <tomas.mattia@gmail.com>
2011-10-27 00:21:06 -0200

    Added only_integer option to ValidateNumericalityOfMatcher

--- lib/shoulda/matchers/active_model/validate_numericality_of_matcher.rb
+++ lib/shoulda/matchers/active_model/validate_numericality_of_matcher.rb
@@ -8,15 +8,22 @@ module Shoulda # :nodoc:
       # * <tt>with_message</tt> - value the test expects to find in
       #   <tt>errors.on(:attribute)</tt>. Regexp or string.  Defaults to the
       #   translation for <tt>:not_a_number</tt>.
+      # * <tt>only_integer</tt> - allows only integer values
       #
-      # Example:
-      #   it { should validate_numericality_of(:age) }
+      # Examples:
+      #   it { should validate_numericality_of(:price) }
+      #   it { should validate_numericality_of(:age).only_integer }
       #
       def validate_numericality_of(attr)
         ValidateNumericalityOfMatcher.new(attr)
       end
 
       class ValidateNumericalityOfMatcher < ValidationMatcher # :nodoc:
+
+        def only_integer
+          @only_integer = true
+          self
+        end
 
         def with_message(message)
           @expected_message = message if message
@@ -25,13 +32,32 @@ module Shoulda # :nodoc:
 
         def matches?(subject)
           super(subject)
-          @expected_message ||= :not_a_number
-          disallows_value_of('abcd', @expected_message)
+          disallows_double_if_only_integer &&
+            disallows_text
         end
 
         def description
-          "only allow numeric values for #{@attribute}"
+          type = if @only_integer then "integer" else "numeric" end
+          description = "only allow #{type} values for #{@attribute}"
+          description
         end
+
+        private
+
+        def disallows_double_if_only_integer
+          if @only_integer
+            message = @expected_message || :not_an_integer
+            disallows_value_of(0.1, message) && disallows_value_of('0.1', message)
+          else
+            true
+          end
+        end
+
+        def disallows_text
+          message = @expected_message || :not_a_number
+          disallows_value_of('abcd', message)
+        end
+
       end
 
     end
