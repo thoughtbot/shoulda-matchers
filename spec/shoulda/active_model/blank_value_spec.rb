d056604d7e0a8d326e427e31a56b35cbadab764f
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-05-25 10:58:07 -0400

    Refer to fully-qualified class name.

--- spec/shoulda/active_model/blank_value_spec.rb
+++ spec/shoulda/active_model/blank_value_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 
 describe Shoulda::Matchers::ActiveModel::BlankValue do
-  subject { Shoulda::Matchers::ActiveModel::BlankValue }
+  let(:matcher_class) { Shoulda::Matchers::ActiveModel::BlankValue }
 
   it "returns an array for collections" do
     model = define_model :parent do
       has_many :children
     end.new
-    subject.new(model, :children).value.should == []
+    matcher_class.new(model, :children).value.should == []
   end
 
   it "returns nil for non collections" do
     model = define_active_model_class("Example", :accessors => [:attr]).new
-    subject.new(model, :attr).value.should be nil
+    matcher_class.new(model, :attr).value.should be nil
   end
 end
 
--- spec/shoulda/active_model/disallow_value_matcher_spec.rb
+++ spec/shoulda/active_model/disallow_value_matcher_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveModel::DisallowValueMatcher do
+  let(:matcher_class) { Shoulda::Matchers::ActiveModel::DisallowValueMatcher }
+
   context "an attribute with a format validation" do
     let(:model) do
       define_model :example, :attr => :string do
@@ -9,11 +11,11 @@ describe Shoulda::Matchers::ActiveModel::DisallowValueMatcher do
     end
 
     it "does not allow a good value" do
-      DisallowValueMatcher.new("abcde").for(:attr).matches?(model).should be_true
+      matcher_class.new("abcde").for(:attr).matches?(model).should be_true
     end
 
     it "allows a bad value" do
-      DisallowValueMatcher.new("xyz").for(:attr).matches?(model).should be_false
+      matcher_class.new("xyz").for(:attr).matches?(model).should be_false
     end
   end
 end
