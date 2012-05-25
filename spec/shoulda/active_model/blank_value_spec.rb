00bca1c8c719d93104d3b6f4ed36ee063d4ef68a
Jason Draper <jason@drapergeek.com>
2012-05-25 09:35:16 -0400

    fix a few broken tests after changing the design of CompositeMatcher

--- spec/shoulda/active_model/blank_value_spec.rb
+++ spec/shoulda/active_model/blank_value_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 
 describe Shoulda::Matchers::ActiveModel::BlankValue do
+  subject { Shoulda::Matchers::ActiveModel::BlankValue }
+
   it "returns an array for collections" do
     model = define_model :parent do
       has_many :children
     end.new
-    BlankValue.new(model, :children).value.should == []
+    subject.new(model, :children).value.should == []
   end
 
   it "returns nil for non collections" do
     model = define_active_model_class("Example", :accessors => [:attr]).new
-    BlankValue.new(model, :attr).value.should be nil
+    subject.new(model, :attr).value.should be nil
   end
 end
 
--- spec/shoulda/active_model/composite_matcher_spec.rb
+++ spec/shoulda/active_model/composite_matcher_spec.rb
@@ -5,7 +5,7 @@ describe Shoulda::Matchers::ActiveModel::CompositeMatcher do
     it 'adds a matcher to match against later' do
       sub_matcher = mock('sub matcher', :matches? => true)
       subject = stub('subject')
-      composite_matcher = CompositeMatcher.new(:attribute)
+      composite_matcher = Shoulda::Matchers::ActiveModel::CompositeMatcher.new
       composite_matcher.add_matcher(sub_matcher)
       composite_matcher.matches?(subject)
       sub_matcher.should have_received(:matches?).with(subject)
@@ -13,8 +13,13 @@ describe Shoulda::Matchers::ActiveModel::CompositeMatcher do
   end
 
   context '#description' do
-    it 'is a generic description designed to be overridden' do
-      CompositeMatcher.new(:attribute).description.should == 'No description'
+    it 'is a combination of descriptions from the sub_matchers' do
+      first_sub_matcher = mock('first sub matchers', :description => 'First Matcher')
+      second_sub_matcher = mock('second sub matchers', :description => 'Second Matcher')
+      composite_matcher = Shoulda::Matchers::ActiveModel::CompositeMatcher.new
+      composite_matcher.add_matcher(first_sub_matcher)
+      composite_matcher.add_matcher(second_sub_matcher)
+      composite_matcher.description.should == "First Matcher Second Matcher"
     end
   end
 
@@ -22,7 +27,7 @@ describe Shoulda::Matchers::ActiveModel::CompositeMatcher do
     it 'returns true if all sub matchers match' do
       sub_matcher = stub('sub matcher', :matches? => true)
       subject = stub('subject')
-      composite_matcher = CompositeMatcher.new(:attribute)
+      composite_matcher = Shoulda::Matchers::ActiveModel::CompositeMatcher.new
       2.times { composite_matcher.add_matcher(sub_matcher) }
       composite_matcher.matches?(subject).should be_true
     end
@@ -31,7 +36,7 @@ describe Shoulda::Matchers::ActiveModel::CompositeMatcher do
       matching_sub_matcher = stub('sub matcher', :matches? => true)
       not_matching_sub_matcher = stub('sub matcher', :matches? => false)
       subject = stub('subject')
-      composite_matcher = CompositeMatcher.new(:attribute)
+      composite_matcher = Shoulda::Matchers::ActiveModel::CompositeMatcher.new
       composite_matcher.add_matcher(matching_sub_matcher)
       composite_matcher.add_matcher(not_matching_sub_matcher)
       composite_matcher.matches?(subject).should be_false
