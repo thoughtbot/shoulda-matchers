6672aae0f732035f8f0e54a6218c10707bba5a45
Fabio Kreusch <fabiokr@gmail.com>
2012-07-04 11:14:30 -0300

    Allows to use the validate_format_of matcher with allow_blank and allow_nil options

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # HEAD
 
+* `validate_format_of` will accept `allow_blank(bool)` and `allow_nil(false)`
+
 * Prefer Test::Unit to Minitest when loading integrations so that RubyMine is
   happy (#88).
 
--- spec/shoulda/active_model/validate_format_of_matcher_spec.rb
+++ spec/shoulda/active_model/validate_format_of_matcher_spec.rb
@@ -35,4 +35,35 @@ describe Shoulda::Matchers::ActiveModel::ValidateFormatOfMatcher do
     end
   end
 
+  context "allowed blank and allowed nil" do
+    context "a postal code" do
+      before do
+        define_model :example, :attr => :string do
+          validates_format_of :attr, :with => /^\d{5}$/, :allow_blank => true, :allow_nil => true
+        end
+        @model = Example.new
+      end
+
+      it "allows allow_blank" do
+        @model.should validate_format_of(:attr).with('12345').allow_blank(true)
+        @model.should validate_format_of(:attr).with('12345').allow_blank()
+        @model.should_not validate_format_of(:attr).with('12345').allow_blank(false)
+
+        @model.should validate_format_of(:attr).not_with('1234').allow_blank(true)
+        @model.should validate_format_of(:attr).not_with('1234').allow_blank()
+        @model.should_not validate_format_of(:attr).not_with('1234').allow_blank(false)
+      end
+
+      it "allows allow_nil" do
+        @model.should validate_format_of(:attr).with('12345').allow_nil(true)
+        @model.should validate_format_of(:attr).with('12345').allow_nil()
+        @model.should_not validate_format_of(:attr).with('12345').allow_nil(false)
+
+        @model.should validate_format_of(:attr).not_with('1234').allow_nil(true)
+        @model.should validate_format_of(:attr).not_with('1234').allow_nil()
+        @model.should_not validate_format_of(:attr).not_with('1234').allow_nil(false)
+      end
+    end
+  end
+
 end
