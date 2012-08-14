657b239b006cdc1f7a05e365623720393985ccb6
Greg Lazarev <greg@thoughtbot.com>
2012-08-14 15:05:26 -0700

    Fixup test descriptions

--- spec/shoulda/active_model/validate_format_of_matcher_spec.rb
+++ spec/shoulda/active_model/validate_format_of_matcher_spec.rb
@@ -9,35 +9,35 @@ describe Shoulda::Matchers::ActiveModel::ValidateFormatOfMatcher do
       @model = Example.new
     end
 
-    it "should be valid" do
+    it "is valid" do
       @model.should validate_format_of(:attr).with('12345')
     end
 
-    it "should not be valid with blank" do
+    it "is not valid with blank" do
       @model.should_not validate_format_of(:attr).with(' ')
       @model.should validate_format_of(:attr).not_with(' ')
     end
 
-    it "should not be valid with nil" do
+    it "is not valid with nil" do
       @model.should_not validate_format_of(:attr).with(nil)
     end
 
-    it "should not be valid with alpha in zip" do
+    it "is not valid with alpha in zip" do
       @model.should_not validate_format_of(:attr).with('1234a')
       @model.should validate_format_of(:attr).not_with('1234a')
     end
 
-    it "should not be valid with too few digits" do
+    it "is not valid with too few digits" do
       @model.should_not validate_format_of(:attr).with('1234')
       @model.should validate_format_of(:attr).not_with('1234')
     end
 
-    it "should not be valid with too many digits" do
+    it "is not valid with too many digits" do
       @model.should_not validate_format_of(:attr).with('123456')
       @model.should validate_format_of(:attr).not_with('123456')
     end
 
-    it "should raise error if you try to call both with and not_with" do
+    it "raises error if you try to call both with and not_with" do
       expect { validate_format_of(:attr).not_with('123456').with('12345') }.
         to raise_error(RuntimeError)
     end
