5380396ec444e78fd57e226b79ace3be72f7fa6f
Greg Lazarev <greg@thoughtbot.com>
2012-08-14 12:54:34 -0700

    Get specs to pass and remove duplicate news

--- spec/shoulda/active_model/validate_format_of_matcher_spec.rb
+++ spec/shoulda/active_model/validate_format_of_matcher_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveModel::ValidateFormatOfMatcher do
-
   context "a postal code" do
     before do
       define_model :example, :attr => :string do
@@ -12,6 +11,15 @@ describe Shoulda::Matchers::ActiveModel::ValidateFormatOfMatcher do
 
     it "should be valid" do
       @model.should validate_format_of(:attr).with('12345')
+    end
+
+    it "should not be valid with blank" do
+      @model.should_not validate_format_of(:attr).with(' ')
+      @model.should validate_format_of(:attr).not_with(' ')
+    end
+
+    it "should not be valid with nil" do
+      @model.should_not validate_format_of(:attr).with(nil)
     end
 
     it "should not be valid with alpha in zip" do
@@ -35,33 +43,35 @@ describe Shoulda::Matchers::ActiveModel::ValidateFormatOfMatcher do
     end
   end
 
-  context "allowed blank and allowed nil" do
-    context "a postal code" do
-      before do
-        define_model :example, :attr => :string do
-          validates_format_of :attr, :with => /^\d{5}$/, :allow_blank => true, :allow_nil => true
-        end
-        @model = Example.new
+  context "when allow_blank and allow_nil are set" do
+    before do
+      define_model :example, :attr => :string do
+        validates_format_of :attr, :with => /^\d{5}$/, :allow_blank => true, :allow_nil => true
       end
+      @model = Example.new
+    end
 
+    it "is valid when attr is nil" do
+      @model.should validate_format_of(:attr).with(nil)
+    end
+
+    it "is valid when attr is blank" do
+      @model.should validate_format_of(:attr).with(' ')
+    end
+
+    describe "#allow_blank" do
       it "allows allow_blank" do
-        @model.should validate_format_of(:attr).with('12345').allow_blank(true)
-        @model.should validate_format_of(:attr).with('12345').allow_blank()
-        @model.should_not validate_format_of(:attr).with('12345').allow_blank(false)
-
-        @model.should validate_format_of(:attr).not_with('1234').allow_blank(true)
-        @model.should validate_format_of(:attr).not_with('1234').allow_blank()
-        @model.should_not validate_format_of(:attr).not_with('1234').allow_blank(false)
+        @model.should validate_format_of(:attr).allow_blank
+        @model.should validate_format_of(:attr).allow_blank(true)
+        @model.should_not validate_format_of(:attr).allow_blank(false)
       end
+    end
 
+    describe "#allow_nil" do
       it "allows allow_nil" do
-        @model.should validate_format_of(:attr).with('12345').allow_nil(true)
-        @model.should validate_format_of(:attr).with('12345').allow_nil()
-        @model.should_not validate_format_of(:attr).with('12345').allow_nil(false)
-
-        @model.should validate_format_of(:attr).not_with('1234').allow_nil(true)
-        @model.should validate_format_of(:attr).not_with('1234').allow_nil()
-        @model.should_not validate_format_of(:attr).not_with('1234').allow_nil(false)
+        @model.should validate_format_of(:attr).allow_nil
+        @model.should validate_format_of(:attr).allow_nil(true)
+        @model.should_not validate_format_of(:attr).allow_nil(false)
       end
     end
   end
