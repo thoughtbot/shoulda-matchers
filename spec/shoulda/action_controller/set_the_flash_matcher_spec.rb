27841b7b4c82de6c67b4047e89f21e411edc3359
grosser <grosser.michael@gmail.com>
2012-08-31 19:10:43 -0700

    Protect against unmatchable classes in flash matcher

--- spec/shoulda/action_controller/set_the_flash_matcher_spec.rb
+++ spec/shoulda/action_controller/set_the_flash_matcher_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActionController::SetTheFlashMatcher do
+  it "should fail with unmatchable to" do
+    expect{
+      set_the_flash.to(1).should
+    }.to raise_error("cannot match against 1")
+  end
+
   context "a controller that sets a flash message" do
     let(:controller) { build_response { flash[:notice] = 'value' } }
 
