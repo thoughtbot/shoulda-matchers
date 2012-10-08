2377fb44aff4da005a3fce6fde43c60bc975149b
Reade Harris <readeharris@gmail.com>
2012-10-08 16:41:39 -0400

    Add #delegate_method matcher.

---
+++ lib/shoulda/matchers/independent.rb
@@ -0,0 +1,9 @@
+require 'shoulda/matchers/independent/delegate_matcher'
+
+module Shoulda
+  module Matchers
+    # = Matchers for plain ol' Ruby objects.
+    module Independent
+    end
+  end
+end
