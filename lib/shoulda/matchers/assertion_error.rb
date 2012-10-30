68ae50045e610188df5ded29b2a37a7dc556357f
Oleg Sukhodolsky <os97673@gmail.com>
2012-10-30 22:46:05 +0400

    #88: Do not force minitest loading when test-unit is available (restoring original fix)

--- lib/shoulda/matchers/assertion_error.rb
+++ lib/shoulda/matchers/assertion_error.rb
@@ -3,7 +3,10 @@ module Shoulda
     if Gem.ruby_version >= Gem::Version.new('1.8') && Gem.ruby_version < Gem::Version.new('1.9')
       require 'test/unit'
       AssertionError = Test::Unit::AssertionFailedError
-    elsif Gem.ruby_version >= Gem::Version.new('1.9')
+    elsif defined?(Test::Unit::AssertionFailedError)
+      # Test::Unit has been loaded already, so we use it
+      AssertionError = Test::Unit::AssertionFailedError
+    elsif Gem.ruby_version >= Gem::Version.new("1.9")
       require 'minitest/unit'
       AssertionError = MiniTest::Assertion
     else
