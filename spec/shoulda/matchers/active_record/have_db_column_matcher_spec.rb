d0ea020d0d644acefe95fbe9ed0f59a2082f5f3f
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-12-23 18:27:26 -0800

    Rework have_db_column matcher

--- spec/shoulda/matchers/active_record/have_db_column_matcher_spec.rb
+++ spec/shoulda/matchers/active_record/have_db_column_matcher_spec.rb
@@ -1,185 +1,111 @@
 require "spec_helper"
 
 describe Shoulda::Matchers::ActiveRecord::HaveDbColumnMatcher do
-  context "have_db_column" do
-    before do
-      @matcher = have_db_column(:nickname)
+  it "accepts an existing database column" do
+    model(:nickname => :string).should have_db_column(:nickname)
+  end
+
+  it "rejects a nonexistent database column" do
+    define_model(:employee).new.should_not have_db_column(:nickname)
+  end
+
+  context "#of_type" do
+    it "accepts a column of correct type" do
+      model(:nickname => :string).should
+        have_db_column(:nickname).of_type(:string)
     end
 
-    it "should accept an existing database column" do
-      create_table 'superheros' do |table|
-        table.string :nickname
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
+    it "rejects a nonexistent database column" do
+      define_model(:superhero).should_not
+        have_db_column(:nickname).of_type(:string)
     end
 
-    it "should reject a nonexistent database column" do
-      define_model :superhero
-      Superhero.new.should_not @matcher
+    it "rejects a column of wrong type" do
+      model(:nickname => :integer).should_not
+        have_db_column(:nickname).of_type(:string)
     end
   end
 
-  context "have_db_column of type string" do
-    before do
-      @matcher = have_db_column(:nickname).of_type(:string)
+  context "with precision option" do
+    it "accepts a column of correct precision" do
+      with_table(:salary, :decimal, :precision => 5).should
+        have_db_column(:salary).with_options(:precision => 5)
     end
 
-    it "should accept a column of correct type" do
-      create_table 'superheros' do |table|
-        table.string :nickname
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a nonexistent database column" do
-      define_model :superhero
-      Superhero.new.should_not @matcher
-    end
-
-    it "should reject a column of wrong type" do
-      create_table 'superheros' do |table|
-        table.integer :nickname
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column of wrong precision" do
+      with_table(:salary, :decimal, :precision => 6).should_not
+        have_db_column(:salary).with_options(:precision => 5)
     end
   end
 
-  context "have_db_column with precision option" do
-    before do
-      @matcher = have_db_column(:salary).with_options(:precision => 5)
+  context "with limit option" do
+    it "accepts a column of correct limit" do
+      with_table(:email, :string, :limit => 255).should
+        have_db_column(:email).with_options(:limit => 255)
     end
 
-    it "should accept a column of correct precision" do
-      create_table 'superheros' do |table|
-        table.decimal :salary, :precision => 5
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a column of wrong precision" do
-      create_table 'superheros' do |table|
-        table.decimal :salary, :precision => 15
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column of wrong limit" do
+      with_table(:email, :string, :limit => 100).should_not
+        have_db_column(:email).with_options(:limit => 255)
     end
   end
 
-  context "have_db_column with limit option" do
-    before do
-      @matcher = have_db_column(:email).
-                   of_type(:string).
-                   with_options(:limit => 255)
+  context "with default option" do
+    it "accepts a column with correct default" do
+      with_table(:admin, :boolean, :default => false).should
+        have_db_column(:admin).with_options(:default => false)
     end
 
-    it "should accept a column of correct limit" do
-      create_table 'superheros' do |table|
-        table.string :email, :limit => 255
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a column of wrong limit" do
-      create_table 'superheros' do |table|
-        table.string :email, :limit => 500
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column with wrong default" do
+      with_table(:admin, :boolean, :default => true).should_not
+        have_db_column(:admin).with_options(:default => false)
     end
   end
 
-  context "have_db_column with default option" do
-    before do
-      @matcher = have_db_column(:admin).
-                   of_type(:boolean).
-                   with_options(:default => false)
+  context "with null option" do
+    it "accepts a column of correct null" do
+      with_table(:admin, :boolean, :null => false).should
+        have_db_column(:admin).with_options(:null => false)
     end
 
-    it "should accept a column of correct default" do
-      create_table 'superheros' do |table|
-        table.boolean :admin, :default => false
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a column of wrong default" do
-      create_table 'superheros' do |table|
-        table.boolean :admin, :default => true
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column of wrong null" do
+      with_table(:admin, :boolean, :null => true).should_not
+        have_db_column(:admin).with_options(:null => false)
     end
   end
 
-  context "have_db_column with null option" do
-    before do
-      @matcher = have_db_column(:admin).
-                   of_type(:boolean).
-                   with_options(:null => false)
+  context "with scale option" do
+    it "accepts a column of correct scale" do
+      with_table(:salary, :decimal, :scale => 2).should
+        have_db_column(:salary).with_options(:scale => 2)
     end
 
-    it "should accept a column of correct null" do
-      create_table 'superheros' do |table|
-        table.boolean :admin, :null => false
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a column of wrong null" do
-      create_table 'superheros' do |table|
-        table.boolean :admin, :null => true
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column of wrong scale" do
+      with_table(:salary, :decimal, :scale => 4).should_not
+        have_db_column(:salary).with_options(:scale => 2)
     end
   end
 
-  context "have_db_column with scale option" do
-    before do
-      @matcher = have_db_column(:salary).
-                   of_type(:decimal).
-                   with_options(:scale => 2)
+  context "with primary option" do
+    it "accepts a column that is primary" do
+      with_table(:id, :integer, :primary => true).should
+        have_db_column(:id).with_options(:primary => true)
     end
 
-    it "should accept a column of correct scale" do
-      create_table 'superheros' do |table|
-        table.decimal :salary, :precision => 10, :scale => 2
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should @matcher
-    end
-
-    it "should reject a column of wrong scale" do
-      create_table 'superheros' do |table|
-        table.decimal :salary, :precision => 10, :scale => 4
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not @matcher
+    it "rejects a column that is not primary" do
+      with_table(:whatever, :integer, :primary => false).should_not
+        have_db_column(:whatever).with_options(:primary => true)
     end
   end
 
-  context "have_db_column with primary option" do
-    it "should accept a column that is primary" do
-      create_table 'superheros' do |table|
-        table.integer :id, :primary => true
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should have_db_column(:id).with_options(:primary => true)
-    end
+  def model(options = {})
+    define_model(:employee, options).new
+  end
 
-    it "should reject a column that is not primary" do
-      create_table 'superheros' do |table|
-        table.integer :primary
-      end
-      define_model_class 'Superhero'
-      Superhero.new.should_not have_db_column(:primary).with_options(:primary => true)
+  def with_table(column_name, column_type, options)
+    create_table "employees" do |table|
+      table.send(column_type, column_name, options)
     end
+    define_model_class("Employee").new
   end
 end
