6125cd1eb48af112b6f2586bdcad2b792b03ad33
Maxim Kaschenko <mkaschenko@me.com>
2012-12-27 21:45:43 +0700

    Removed ValidateFormatOfMatcher#not_with

--- lib/shoulda/matchers/active_model/validate_format_of_matcher.rb
+++ lib/shoulda/matchers/active_model/validate_format_of_matcher.rb
@@ -49,32 +49,15 @@ module Shoulda # :nodoc:
         end
 
         def with(value)
-          if @value_to_fail
-            raise 'You may not call both with and not_with'
-          else
-            @value_to_pass = value
-            self
-          end
-        end
-
-        def not_with(value)
-          if @value_to_pass
-            raise 'You may not call both with and not_with'
-          else
-            @value_to_fail = value
-            self
-          end
+          @value_to_pass = value
+          self
         end
 
         def matches?(subject)
           super(subject)
           @expected_message ||= :invalid
 
-          if @value_to_fail
-            disallows_value_of(@value_to_fail, @expected_message) && allows_blank_value? && allows_nil_value?
-          else
-            allows_value_of(@value_to_pass, @expected_message) && allows_blank_value? && allows_nil_value?
-          end
+          allows_value_of(@value_to_pass, @expected_message) && allows_blank_value? && allows_nil_value?
         end
 
         def description
