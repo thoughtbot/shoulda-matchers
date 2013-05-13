9fb69af1c4dc614286badb43d91de9e9019540ae
Tiago Cardoso <tiago@restorm.com>
2013-05-13 12:06:32 +0200

    i think the problem was that i was assign explicitly in the it clause. hope this is more in sync with the other specs

--- spec/shoulda/matchers/active_model/comparison_matcher_spec.rb
+++ spec/shoulda/matchers/active_model/comparison_matcher_spec.rb
@@ -1,55 +1,43 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveModel::ComparisonMatcher do
-  subject { comparison_instance }
   context 'with a model with a greater than validation' do
     it 'match accordingly' do
-      subject.attr = 3
-      subject.should matcher(:greater_than, 2)
-      subject.attr = 2
-      subject.should_not matcher(:greater_than, 2)
-      subject.attr = 1
-      subject.should_not matcher(:greater_than, 2)
+      comparison_instance(3).should matcher(:greater_than, 2)
+      comparison_instance(2).should_not matcher(:greater_than, 2)
+      comparison_instance(1).should_not matcher(:greater_than, 2)
     end
   end
 
   context 'with a model with a greater than or equal to validation' do
     it 'match accordingly' do
-      subject.attr = 3
-      subject.should matcher(:greater_than_or_equal_to, 2)
-      subject.attr = 2
-      subject.should matcher(:greater_than_or_equal_to, 2)
-      subject.attr = 1
-      subject.should_not matcher(:greater_than_or_equal_to, 2)
+      comparison_instance(3).should matcher(:greater_than_or_equal_to, 2)
+      comparison_instance(2).should matcher(:greater_than_or_equal_to, 2)
+      comparison_instance(1).should_not matcher(:greater_than_or_equal_to, 2)
     end
   end
 
   context 'with a model with a less than validation' do
     it 'match accordingly' do
-      subject.attr = 3
-      subject.should_not matcher(:less_than, 2)
-      subject.attr = 2
-      subject.should_not matcher(:less_than, 2)
-      subject.attr = 1
-      subject.should matcher(:less_than, 2)
+      comparison_instance(3).should_not matcher(:less_than, 2)
+      comparison_instance(2).should_not matcher(:less_than, 2)
+      comparison_instance(1).should matcher(:less_than, 2)
     end
   end
 
   context 'with a model with a less than or equal to validation' do
     it 'match accordingly' do
-      subject.attr = 3
-      subject.should_not matcher(:less_than_or_equal_to, 2)
-      subject.attr = 2
-      subject.should matcher(:less_than_or_equal_to, 2)
-      subject.attr = 1
-      subject.should matcher(:less_than_or_equal_to, 2)
+      comparison_instance(3).should_not matcher(:less_than_or_equal_to, 2)
+      comparison_instance(2).should matcher(:less_than_or_equal_to, 2)
+      comparison_instance(1).should matcher(:less_than_or_equal_to, 2)
     end
   end
 
-  def comparison_instance(options = {})
+  def comparison_instance(attr, options = {})
     define_model :example, :attr => :string do
       validates_numericality_of :attr, options
-    end.new
+      attr_accessible :attr
+    end.new(:attr => attr)
   end
 
   def matcher(match, val)
