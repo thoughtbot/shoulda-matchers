5ae1dd5230e87d1186db0a987cc18217c1a4e2e2
Tiago Cardoso <tiago@restorm.com>
2013-05-20 14:12:12 +0200

    made the spec more readable

--- spec/shoulda/matchers/active_model/comparison_matcher_spec.rb
+++ spec/shoulda/matchers/active_model/comparison_matcher_spec.rb
@@ -2,27 +2,23 @@ require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveModel::ComparisonMatcher do
   context 'with a model with a greater than validation' do
-    it 'match accordingly' do
-      comparison_instance(:greater_than => 2).should matcher.is_greater_than(2)
-    end
+    subject{ comparison_instance(:greater_than => 2) }
+    it { should matcher.is_greater_than(2) }
   end
 
   context 'with a model with a greater than or equal to validation' do
-    it 'match accordingly' do
-      comparison_instance(:greater_than_or_equal_to => 2).should matcher.is_greater_than_or_equal_to(2)
-    end
+    subject { comparison_instance(:greater_than_or_equal_to => 2) }
+    it { should matcher.is_greater_than_or_equal_to(2) }
   end
 
   context 'with a model with a less than validation' do
-    it 'match accordingly' do
-      comparison_instance(:less_than => 2).should matcher.is_less_than(2)
-    end
+    subject { comparison_instance(:less_than => 2) }
+    it { should matcher.is_less_than(2) }
   end
 
   context 'with a model with a less than or equal to validation' do
-    it 'match accordingly' do
-      comparison_instance(:less_than_or_equal_to => 2).should matcher.is_less_than_or_equal_to(2)
-    end
+    subject { comparison_instance(:less_than_or_equal_to => 2) }
+    it { should matcher.is_less_than_or_equal_to(2) }
   end
 
   def comparison_instance(options = {})
