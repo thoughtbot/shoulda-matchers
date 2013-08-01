d49c25e53b30fcc9b36ca599c899ca8795ae98d0
randoum <randoum@gmail.com>
2013-08-02 06:10:36 +0800

    Create validate_absence_of_matcher.rb

---
+++ lib/shoulda/matchers/active_model/validate_absence_of_matcher.rb
@@ -0,0 +1,59 @@
+module Shoulda # :nodoc:
+  module Matchers
+    module ActiveModel # :nodoc:
+
+      # Ensures that the model is not valid if the given attribute is
+      # present. Work by default with string/text columns.
+      #
+      # Options:
+      # * <tt>with_message</tt> - value the test expects to find in
+      # * <tt>of_type</tt> - db column type (:integer or :string)
+      # * <tt>errors.on(:attribute)</tt>. <tt>Regexp</tt> or <tt>String</tt>.
+      #   Defaults to the translation for <tt>:present</tt>.
+      #
+      # Examples:
+      #   it { should validate_absence_of(:name) }
+      #   it { should validate_absence_of(:user_id).
+      #                 of_type(:integer) }
+      #   it { should validate_absence_of(:name).
+      #                 with_message(/must be left blank/) }
+      #
+      def validate_absence_of(attr)
+        ValidateAbsenceOfMatcher.new(attr)
+      end
+
+      class ValidateAbsenceOfMatcher < ValidationMatcher # :nodoc:
+
+        def with_message(message)
+          @expected_message = message if message
+          self
+        end
+
+        def of_type(type)
+          @type = type if type
+        end
+
+        def matches?(subject)
+          super(subject)
+          @expected_message ||= :present
+          @type ||= :string
+          disallows_value_of(set_value, @expected_message)
+        end
+
+        def description
+          "require #{@attribute} not to be set"
+        end
+
+        private
+
+        def set_value
+          if @type == :string
+            'a'
+          else
+            1
+          end
+        end
+      end
+    end
+  end
+end
