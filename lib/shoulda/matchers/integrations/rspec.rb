413c812ae9d6424a6fb51bc5579e5ad2065208bc
Adil Jarulin <adil.jarulin@gmail.com>
2013-08-13 17:36:22 +0600

    allow to include shoulda modules using the :type option

--- lib/shoulda/matchers/integrations/rspec.rb
+++ lib/shoulda/matchers/integrations/rspec.rb
@@ -6,24 +6,21 @@ RSpec.configure do |config|
     require 'shoulda/matchers/active_record'
     require 'shoulda/matchers/active_model'
 
-    config.include Shoulda::Matchers::ActiveRecord , :example_group => {
-      :file_path => /spec\/models/
-    }
-    config.include Shoulda::Matchers::ActiveModel, :example_group => {
-      :file_path => /spec\/models/
-    }
+    config.include Shoulda::Matchers::ActiveRecord , :type => :model, 
+      :example_group => { :file_path => /spec\/models/ }
+
+    config.include Shoulda::Matchers::ActiveModel, :type => :model,
+      :example_group => { :file_path => /spec\/models/ }
 
   elsif defined?(::ActiveModel)
     require 'shoulda/matchers/active_model'
-    config.include Shoulda::Matchers::ActiveModel, :example_group => {
-      :file_path => /spec\/models/
-    }
+    config.include Shoulda::Matchers::ActiveModel, :type => :model,
+      :example_group => { :file_path => /spec\/models/ }
   end
 
   if defined?(::ActionController)
     require 'shoulda/matchers/action_controller'
-    config.include Shoulda::Matchers::ActionController, :example_group => {
-      :file_path => /spec\/controllers/
-    }
+    config.include Shoulda::Matchers::ActionController, :type => :controller,
+      :example_group => { :file_path => /spec\/controllers/ }
   end
 end
