7c7c79235da8dc4113c9a9518ebcc6b8088f572b
Elliot Winkler <elliot.winkler@gmail.com>
2013-08-15 18:11:01 -0600

    Clean up Cucumber config so we can generate other projects
    In addition to writing tests against Rails projects we want to also
    write tests against more plain Ruby projects. There are global variables
    and behavior which can be shared between these two kinds of tests.

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -1,23 +1,9 @@
-PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
-APP_NAME     = 'testapp'.freeze
-
-BUNDLE_ENV_VARS = %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE)
-ORIGINAL_BUNDLE_VARS = Hash[ENV.select{ |key,value| BUNDLE_ENV_VARS.include?(key) }]
-
-Before do
-  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, ENV['BUNDLE_GEMFILE']) unless ENV['BUNDLE_GEMFILE'].start_with?(Dir.pwd)
-end
-
-After do
-  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
-    ENV[key] = value
-  end
-end
+RAILS_APP_NAME = 'testapp'.freeze
 
 When 'I generate a new rails application' do
   steps %{
-    When I run `rails new #{APP_NAME}`
-    And I cd to "#{APP_NAME}"
+    When I run `rails new #{RAILS_APP_NAME}`
+    And I cd to "#{RAILS_APP_NAME}"
     And I comment out the gem "turn" from the Gemfile
     And I comment out the gem "coffee-rails" from the Gemfile
     And I comment out the gem "uglifier" from the Gemfile
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,17 @@
+PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
+
+BUNDLE_ENV_VARS = %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE)
+ORIGINAL_BUNDLE_VARS = Hash[ENV.select{ |key,value| BUNDLE_ENV_VARS.include?(key) }]
+
 require 'aruba/cucumber'
 
 Before do
   @aruba_timeout_seconds = 60 * 2
+  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, ENV['BUNDLE_GEMFILE']) unless ENV['BUNDLE_GEMFILE'].start_with?(Dir.pwd)
+end
+
+After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
+  end
 end
