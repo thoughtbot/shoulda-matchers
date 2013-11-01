82abb98c1d62751a11ce901a07424fa37b5516d7
Elliot Winkler <elliot.winkler@gmail.com>
2013-11-01 09:28:13 -0600

    Fix names of tests

--- spec/shoulda/matchers/active_record/association_matchers/model_reflection_spec.rb
+++ spec/shoulda/matchers/active_record/association_matchers/model_reflection_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection do
-  it "delegates other methods to the given Reflection object" do
+  it 'delegates other methods to the given Reflection object' do
     define_model(:country)
     person_model = define_model(:person, country_id: :integer) do
       belongs_to :country
@@ -14,7 +14,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
   end
 
   describe '#associated_class' do
-    it "returns the model that the association refers to" do
+    it 'returns the model that the association refers to' do
       define_model(:country)
       person_model = define_model(:person, country_id: :integer) do
         belongs_to :country
@@ -27,7 +27,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
   end
 
   describe '#through?' do
-    it "returns true if the reflection is for a has_many :through association" do
+    it 'returns true if the reflection is for a has_many :through association' do
       define_model(:city, person_id: :integer)
       define_model(:person, country_id: :integer) do
         has_many :cities
@@ -42,7 +42,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
       expect(reflection).to be_through
     end
 
-    it "returns false if not" do
+    it 'returns false if not' do
       define_model(:person, country_id: :integer)
       country_model = define_model(:country) do
         has_many :people
@@ -56,7 +56,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
 
   describe '#join_table' do
     context 'when the association was defined with a :join_table option' do
-      it "returns the value" do
+      it 'returns the value of the option' do
         define_model(:person, country_id: :integer)
         country_model = define_model(:country) do
           has_and_belongs_to_many :people, join_table: 'foos'
@@ -69,7 +69,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
     end
 
     context 'when the association was not defined with :join_table' do
-      it "returns the default value" do
+      it 'returns the default join_table that ActiveRecord generates' do
         define_model(:person, country_id: :integer)
         country_model = define_model(:country) do
           has_and_belongs_to_many :people
@@ -86,7 +86,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
     if rails_4_x?
       context 'when the reflection object has a #scope method' do
         context 'when the scope is a block' do
-          it "executes the block in the context of an empty scope" do
+          it 'executes the block in the context of an empty scope' do
             define_model(:country, mood: :string)
             person_model = define_model(:person, country_id: :integer) do
               belongs_to :country, -> { where(mood: 'nice') }
@@ -101,7 +101,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the scope is nil' do
-          it "returns an empty scope" do
+          it 'returns an empty scope' do
             define_model(:country)
             person_model = define_model(:person, country_id: :integer) do
               belongs_to :country
@@ -120,7 +120,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
     if rails_3_x?
       context 'when the reflection object does not have a #scope method' do
         context 'when the reflection options contain :conditions' do
-          it "creates an ActiveRecord::Relation from the conditions" do
+          it 'creates an ActiveRecord::Relation from the conditions' do
             define_model(:country, mood: :string)
             person_model = define_model(:person, country_id: :integer) do
               belongs_to :country, conditions: { mood: 'nice' }
@@ -135,7 +135,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :order' do
-          it "creates an ActiveRecord::Relation from the order" do
+          it 'creates an ActiveRecord::Relation from the order' do
             define_model(:person, country_id: :integer, age: :integer)
             country_model = define_model(:country) do
               has_many :people, order: 'age'
@@ -150,7 +150,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :include' do
-          it "creates an ActiveRecord::Relation from the include" do
+          it 'creates an ActiveRecord::Relation from the include' do
             define_model(:city, country_id: :integer)
             define_model(:country) do
               has_many :cities
@@ -168,7 +168,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :group' do
-          it "creates an ActiveRecord::Relation from the group" do
+          it 'creates an ActiveRecord::Relation from the group' do
             country_model = define_model(:country, mood: :string) do
               has_many :people, group: 'age'
             end
@@ -183,7 +183,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :having' do
-          it "creates an ActiveRecord::Relation from the having" do
+          it 'creates an ActiveRecord::Relation from the having' do
             country_model = define_model(:country) do
               has_many :people, having: 'country_id > 1'
             end
@@ -198,7 +198,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :limit' do
-          it "creates an ActiveRecord::Relation from the limit" do
+          it 'creates an ActiveRecord::Relation from the limit' do
             country_model = define_model(:country) do
               has_many :people, limit: 10
             end
@@ -213,7 +213,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :offset' do
-          it "creates an ActiveRecord::Relation from the limit" do
+          it 'creates an ActiveRecord::Relation from the offset' do
             country_model = define_model(:country) do
               has_many :people, offset: 5
             end
@@ -228,7 +228,7 @@ describe Shoulda::Matchers::ActiveRecord::AssociationMatchers::ModelReflection d
         end
 
         context 'when the reflection options contain :select' do
-          it "creates an ActiveRecord::Relation from the limit" do
+          it 'creates an ActiveRecord::Relation from the select' do
             country_model = define_model(:country) do
               has_many :people, select: 'age'
             end
