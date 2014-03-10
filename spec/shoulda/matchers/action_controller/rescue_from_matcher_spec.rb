eb8e3e07b4226d9a899649f6881cd4ac29e0d33f
Dmitry Tonkonogov <omun.spambox@gmail.com>
2014-03-10 13:18:12 +1100

    Replace double quotes for single ones

--- spec/shoulda/matchers/action_controller/rescue_from_matcher_spec.rb
+++ spec/shoulda/matchers/action_controller/rescue_from_matcher_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Shoulda::Matchers::ActionController::RescueFromMatcher do
   context 'a controller that rescues from RuntimeError' do
-    it "asserts controller is setup with rescue_from" do
+    it 'asserts controller is setup with rescue_from' do
       expect(controller_with_rescue_from).to rescue_from RuntimeError
     end
 
     context 'with a handler method' do
       shared_examples 'handler is correct' do
-        it "asserts rescue_from was set up with handler method" do
+        it 'asserts rescue_from was set up with handler method' do
           expect(controller).to rescue_from(RuntimeError).with(:error_method)
         end
       end
@@ -28,11 +28,11 @@ describe Shoulda::Matchers::ActionController::RescueFromMatcher do
         it_behaves_like 'handler is correct'
       end
 
-      it "asserts rescue_from was not set up with incorrect handler method" do
+      it 'asserts rescue_from was not set up with incorrect handler method' do
         expect(controller_with_rescue_from_and_method).not_to rescue_from(RuntimeError).with(:other_method)
       end
 
-      it "asserts the controller responds to the handler method" do
+      it 'asserts the controller responds to the handler method' do
         matcher = rescue_from(RuntimeError).with(:error_method)
         expect(matcher.matches?(controller_with_rescue_from_and_invalid_method)).to eq false
         expect(matcher.failure_message).to match(/does not respond to/)
@@ -40,7 +40,7 @@ describe Shoulda::Matchers::ActionController::RescueFromMatcher do
     end
 
     context 'without a handler method' do
-      it "the handler method is not included in the description" do
+      it 'the handler method is not included in the description' do
         matcher = rescue_from(RuntimeError)
         expect(matcher.matches?(controller_with_rescue_from)).to eq true
         expect(matcher.description).not_to match(/with #/)
@@ -49,21 +49,21 @@ describe Shoulda::Matchers::ActionController::RescueFromMatcher do
   end
 
   context 'a controller that does not rescue from RuntimeError' do
-    it "asserts controller is not setup with rescue_from" do
+    it 'asserts controller is not setup with rescue_from' do
       matcher = rescue_from RuntimeError
-      expect(define_controller("RandomController")).not_to matcher
+      expect(define_controller('RandomController')).not_to matcher
       expect(matcher.failure_message_when_negated).to match(/Did not expect \w+ to rescue from/)
     end
   end
 
   def controller_with_rescue_from
-    define_controller "RescueRuntimeError" do
+    define_controller 'RescueRuntimeError' do
       rescue_from(RuntimeError) {}
     end
   end
 
   def controller_with_rescue_from_and_invalid_method
-    define_controller "RescueRuntimeErrorWithMethod" do
+    define_controller 'RescueRuntimeErrorWithMethod' do
       rescue_from RuntimeError, with: :error_method
     end
   end
