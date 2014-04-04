cd9594603eb9485deb63f3ce107e4ebf490efbf5
Elliot Winkler <elliot.winkler@gmail.com>
2014-04-04 15:02:18 -0600

    Remove warning under Minitest 5
    Under Minitest 5 (or a Rails 4.1 project), you will get a warning
    
        MiniTest::Unit::TestCase is now Minitest::Test.
    
    when you attempt to run tests. This is because shoulda-matchers requires
    'test/unit/testcase', even if Test::Unit is not being used.
    
    It is not a good idea for shoulda-matchers to require *anything* that
    the user may not be using, whether it's Test::Unit or RSpec. We should
    assume that by the time shoulda-matchers is required, the user has
    already required all of whatever test framework they are using, not none
    or even some of that framework.

--- lib/shoulda/matchers.rb
+++ lib/shoulda/matchers.rb
@@ -4,6 +4,20 @@ require 'shoulda/matchers/rails_shim'
 require 'shoulda/matchers/warn'
 require 'shoulda/matchers/version'
 
+require 'shoulda/matchers/independent'
+
+if defined?(ActiveModel)
+  require 'shoulda/matchers/active_model'
+end
+
+if defined?(ActiveRecord)
+  require 'shoulda/matchers/active_record'
+end
+
+if defined?(ActionController)
+  require 'shoulda/matchers/action_controller'
+end
+
 if defined?(RSpec)
   require 'shoulda/matchers/integrations/rspec'
 end
---
+++ lib/shoulda/matchers/integrations/nunit_test_case_detection.rb
@@ -0,0 +1,36 @@
+module Shoulda
+  module Matchers
+    module Integrations
+      module NUnitTestCaseDetection
+        def self.possible_test_case_constants
+          [
+            -> { ActiveSupport::TestCase },
+            -> { Minitest::Test },
+            -> { MiniTest::Unit::TestCase },
+            -> { Test::Unit::TestCase }
+          ]
+        end
+
+        def self.resolve_constant(future_constant)
+          future_constant.call
+        rescue NameError
+          nil
+        end
+
+        def self.detected_test_case_constants
+          possible_test_case_constants.
+            map { |future_constant| resolve_constant(future_constant) }.
+            compact
+        end
+
+        def self.test_case_constants
+          @_test_case_constants ||= detected_test_case_constants
+        end
+      end
+    end
+
+    def self.nunit_test_case_constants
+      Integrations::NUnitTestCaseDetection.test_case_constants
+    end
+  end
+end
