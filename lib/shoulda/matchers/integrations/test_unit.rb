0ac771687f956399550cd31d256ebde8851ae00c
Anthony Williams <hi@antw.me>
2014-04-15 14:16:32 +0100

    Only include ActiveRecord matchers if AR is defined
    Fixes cases where you may be using ActiveModel without ActiveRecord.
    
    Closes #480.

--- lib/shoulda/matchers/integrations/test_unit.rb
+++ lib/shoulda/matchers/integrations/test_unit.rb
@@ -21,12 +21,14 @@ end
 
 if defined?(ActiveSupport::TestCase)
   ActiveSupport::TestCase.class_eval do
-    include Shoulda::Matchers::ActiveRecord
-    extend Shoulda::Matchers::ActiveRecord
-  end
+    if defined?(ActiveRecord)
+      include Shoulda::Matchers::ActiveRecord
+      extend Shoulda::Matchers::ActiveRecord
+    end
 
-  ActiveSupport::TestCase.class_eval do
-    include Shoulda::Matchers::ActiveModel
-    extend Shoulda::Matchers::ActiveModel
+    if defined?(ActiveModel)
+      include Shoulda::Matchers::ActiveModel
+      extend Shoulda::Matchers::ActiveModel
+    end
   end
 end
