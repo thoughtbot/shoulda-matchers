1278a7047b71609850c4f347a0d2819b3c2dd7b6
Maxim Kaschenko <mkaschenko@me.com>
2014-05-10 16:06:19 +0700

    Rename references within validate_exclusion_of_matcher_spec.rb

--- spec/shoulda/matchers/active_model/validate_exclusion_of_matcher_spec.rb
+++ spec/shoulda/matchers/active_model/validate_exclusion_of_matcher_spec.rb
@@ -1,39 +1,39 @@
 require 'spec_helper'
 
-describe Shoulda::Matchers::ActiveModel::EnsureExclusionOfMatcher do
+describe Shoulda::Matchers::ActiveModel::ValidateExclusionOfMatcher do
   context 'an attribute which must be excluded from a range' do
     it 'accepts ensuring the correct range' do
       expect(validating_exclusion(in: 2..5)).
-        to ensure_exclusion_of(:attr).in_range(2..5)
+        to validate_exclusion_of(:attr).in_range(2..5)
     end
 
     it 'rejects ensuring excluded value' do
       expect(validating_exclusion(in: 2..5)).
-        not_to ensure_exclusion_of(:attr).in_range(2..6)
+        not_to validate_exclusion_of(:attr).in_range(2..6)
     end
 
     it 'does not override the default message with a blank' do
       expect(validating_exclusion(in: 2..5)).
-        to ensure_exclusion_of(:attr).in_range(2..5).with_message(nil)
+        to validate_exclusion_of(:attr).in_range(2..5).with_message(nil)
     end
   end
 
   context 'an attribute which must be excluded from a range with excluded end' do
     it 'accepts ensuring the correct range' do
       expect(validating_exclusion(in: 2...5)).
-        to ensure_exclusion_of(:attr).in_range(2...5)
+        to validate_exclusion_of(:attr).in_range(2...5)
     end
 
     it 'rejects ensuring excluded value' do
       expect(validating_exclusion(in: 2...5)).
-        not_to ensure_exclusion_of(:attr).in_range(2...4)
+        not_to validate_exclusion_of(:attr).in_range(2...4)
     end
   end
 
   context 'an attribute with a custom validation message' do
     it 'accepts ensuring the correct range' do
       expect(validating_exclusion(in: 2..4, message: 'not good')).
-        to ensure_exclusion_of(:attr).in_range(2..4).with_message(/not good/)
+        to validate_exclusion_of(:attr).in_range(2..4).with_message(/not good/)
     end
   end
 
@@ -45,7 +45,7 @@ describe Shoulda::Matchers::ActiveModel::EnsureExclusionOfMatcher do
         end
       end
 
-      expect(model).to ensure_exclusion_of(:attr).in_range(2..5).
+      expect(model).to validate_exclusion_of(:attr).in_range(2..5).
         with_message(/should be out of this range/)
 
       model = custom_validation do
@@ -54,7 +54,7 @@ describe Shoulda::Matchers::ActiveModel::EnsureExclusionOfMatcher do
         end
       end
 
-      expect(model).to ensure_exclusion_of(:attr).in_range(2...5).
+      expect(model).to validate_exclusion_of(:attr).in_range(2...5).
         with_message(/should be out of this range/)
     end
   end
@@ -62,21 +62,21 @@ describe Shoulda::Matchers::ActiveModel::EnsureExclusionOfMatcher do
   context 'an attribute which must be excluded from an array' do
     it 'accepts with correct array' do
       expect(validating_exclusion(in: %w(one two))).
-        to ensure_exclusion_of(:attr).in_array(%w(one two))
+        to validate_exclusion_of(:attr).in_array(%w(one two))
     end
 
     it 'rejects when only part of array matches' do
       expect(validating_exclusion(in: %w(one two))).
-        not_to ensure_exclusion_of(:attr).in_array(%w(one wrong_value))
+        not_to validate_exclusion_of(:attr).in_array(%w(one wrong_value))
     end
 
     it 'rejects when array does not match at all' do
       expect(validating_exclusion(in: %w(one two))).
-        not_to ensure_exclusion_of(:attr).in_array(%w(cat dog))
+        not_to validate_exclusion_of(:attr).in_array(%w(cat dog))
     end
 
     it 'has correct description' do
-      expect(ensure_exclusion_of(:attr).in_array([true, 'dog']).description).
+      expect(validate_exclusion_of(:attr).in_array([true, 'dog']).description).
         to eq 'ensure exclusion of attr in [true, "dog"]'
     end
 
