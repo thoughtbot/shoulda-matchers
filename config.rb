900533530a2e17d8d16c93014eba8a3517c720cd
Elliot Winkler <elliot.winkler@gmail.com>
2015-01-23 16:00:53 -0700

    Use relative assets
    This way the site will render on both thoughtbot.github.io as well as
    shoulda.io.

--- config.rb
+++ config.rb
@@ -74,8 +74,8 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "/shoulda-matchers"
+  # set :http_prefix, "/shoulda-matchers"
 end
