b4b4f022c4a4906e54f27e66c19e1b5b69c7dbd7
Elliot Winkler <elliot.winkler@gmail.com>
2015-02-08 15:23:10 -0700

    Stop testing against columns unsupported by SQLite

--- spec/support/tests/database_configuration.rb
+++ spec/support/tests/database_configuration.rb
@@ -5,10 +5,17 @@ module Tests
   class DatabaseConfiguration < SimpleDelegator
     ENVIRONMENTS = %w(development test production)
 
+    attr_reader :adapter_class
+
     def self.for(database_name, adapter_name)
       config_class = DatabaseConfigurationRegistry.instance.get(adapter_name)
       config = config_class.new(database_name)
       new(config)
+    end
+
+    def initialize(config)
+      @adapter_class = config.class.to_s.split('::').last
+      super(config)
     end
 
     def to_hash
