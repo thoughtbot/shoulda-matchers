c4234af6301f3f5332df71463244779976e8e8c0
Albert Wang <albert@cellabus.com>
2015-10-04 19:04:41 -0700

    Add CouldNotValidateCaseError

--- lib/shoulda/matchers/active_model/errors.rb
+++ lib/shoulda/matchers/active_model/errors.rb
@@ -47,6 +47,46 @@ Please use a #{record_name} with an empty `password` instead.
           model_name.humanize.downcase
         end
       end
+
+      class CouldNotValidateCaseError < Shoulda::Matchers::Error
+        def self.create(record, attribute)
+          super(record: record, attribute: attribute)
+        end
+
+        attr_accessor :record, :attribute
+
+        def message
+          <<-EOT.strip
+Your #{model} model has a uniqueness validation on :#{attribute} which is
+declared to be case-sensitive, but the value the uniqueness matcher used,
+"#{value}", doesn't contain any alpha characters, so using it to test the
+case-sensitivity part of the validation is ineffective. There are two possible
+solutions for this depending on what you're trying to do here:
+
+If you meant for the validation to be case-sensitive,
+then you need to give the uniqueness matcher a saved instance of #{model} with a
+value for :#{attribute} that contains alpha characters.
+
+If you meant for the validation to be case-insensitive,
+then you need to add `case_sensitive: false` to the validation and add
+case_insensitive to the matcher.
+
+For full examples, please see:
+
+http://github.com/thoughtbot/shoulda-matchers/wiki/error-uniques
+          EOT
+        end
+
+        private
+
+        def model
+          record.class
+        end
+
+        def value
+          record.__send__(@attribute)
+        end
+      end
     end
   end
 end
