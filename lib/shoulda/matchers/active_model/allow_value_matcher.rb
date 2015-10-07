ba953f8698f0f8ef61454c0cb92797b0a99f7160
Elliot Winkler <elliot.winkler@gmail.com>
2015-10-06 23:10:51 -0600

    Update CouldNotSetAttributeError message to be more useful

--- lib/shoulda/matchers/active_model/allow_value_matcher.rb
+++ lib/shoulda/matchers/active_model/allow_value_matcher.rb
@@ -326,7 +326,29 @@ module Shoulda
           attr_accessor :model, :attribute, :expected_value, :actual_value
 
           def message
-            "Expected #{model.class} to be able to set #{attribute} to #{expected_value.inspect}, but got #{actual_value.inspect} instead."
+            Shoulda::Matchers.word_wrap <<-MESSAGE
+The allow_value matcher attempted to set :#{attribute} on #{model.name} to
+#{expected_value.inspect}, but when the attribute was read back, it
+had stored #{actual_value.inspect} instead.
+
+This creates a problem because it means that the model is behaving in a way that
+is interfering with the test -- there's a mismatch between the test that was
+written and test that was actually run.
+
+There are a couple of reasons why this could be happening:
+
+* The writer method for :#{attribute} has been overridden and contains custom
+  logic to prevent certain values from being set or change which values are
+  stored.
+* ActiveRecord is typecasting the incoming value.
+
+Regardless, the fact you're seeing this message usually indicates a larger
+problem. Please file an issue on the GitHub repo for shoulda-matchers,
+including details about your model and the test you've written, and we can point
+you in the right direction:
+
+https://github.com/thoughtbot/shoulda-matchers/issues
+            MESSAGE
           end
         end
 
