d7ac9cb9704763827b2a2d174af06dd9129746aa
Michael Deering <mdeering@mdeering.com>
2015-10-25 21:14:07 -0600

    Do not apply .railsrc

--- spec/support/acceptance/helpers/step_helpers.rb
+++ spec/support/acceptance/helpers/step_helpers.rb
@@ -64,7 +64,7 @@ module AcceptanceTests
     def create_rails_application
       fs.clean
 
-      command = "bundle exec rails new #{fs.project_directory} --skip-bundle"
+      command = "bundle exec rails new #{fs.project_directory} --skip-bundle --no-rc"
 
       run_command!(command) do |runner|
         runner.directory = nil
