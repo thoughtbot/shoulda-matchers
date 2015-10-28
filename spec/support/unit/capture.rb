50a8c0a84e04e12639dd816911408ecc3306346b
Elliot Winkler <elliot.winkler@gmail.com>
2015-10-27 23:38:08 -0600

    Remove warnings shown when running tests
    * For some reason, ActiveRecord was printing a warning saying that
      "active_record/base" was already being loaded and there was a possible
      circular dependency. We fix this by explicitly requiring
      "active_record/base" before `$VERBOSE` is set to true.
    * We were getting warnings when re-defining Kernel.capture,
      Kernel.silence_stream and Kernel.silence_stderr, so undefine them
      first.

--- spec/support/unit/capture.rb
+++ spec/support/unit/capture.rb
@@ -2,6 +2,8 @@ module Kernel
   # #capture, #silence_stream, and #silence_stderr are deprecated after Rails
   # 4.2 and will be removed in 5.0, so just override them completely here
 
+  undef_method :capture
+
   def capture(stream)
     stream = stream.to_s
     captured_stream = Tempfile.new(stream)
@@ -18,6 +20,8 @@ module Kernel
     stream_io.reopen(origin_stream)
   end
 
+  undef_method :silence_stream
+
   def silence_stream(stream)
     old_stream = stream.dup
     stream.reopen(RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL:' : '/dev/null')
@@ -27,6 +31,8 @@ module Kernel
     stream.reopen(old_stream)
     old_stream.close
   end
+
+  undef_method :silence_stderr
 
   def silence_stderr
     silence_stream(STDERR) { yield }
