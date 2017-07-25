2dfd855c63aae333ce867a2ae0a13073faa394a6
Elliot Winkler <elliot.winkler@gmail.com>
2017-07-24 21:15:24 -0500

    Update rspec & rspec-rails to 3.6.x
    This upgrade is mainly so we can use --only-failures which was
    introduced way back in RSpec 3.3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,14 @@ require 'pry-byebug'
 require 'rspec'
 
 RSpec.configure do |config|
-  config.order = :random
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 
+  config.order = :random
   config.default_formatter = 'doc'
-
   config.mock_with :rspec
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
 $VERBOSE = true
