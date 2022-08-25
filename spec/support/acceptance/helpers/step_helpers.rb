b15a0f9018561689f7e5aed10360982a3ca61550
Vít Ondruch <vondruch@redhat.com>
2022-08-25 16:07:57 +0200

    Using local gems should be enough for testing (#1504)

--- spec/support/acceptance/helpers/step_helpers.rb
+++ spec/support/acceptance/helpers/step_helpers.rb
@@ -116,7 +116,7 @@ module AcceptanceTests
 
     def add_rspec_rails_to_project!
       add_gem 'rspec-rails', rspec_rails_version
-      run_command_within_bundle!('bundle install --binstubs') if rails_gt_6_0?
+      run_command_within_bundle!('bundle install --local --binstubs') if rails_gt_6_0?
       run_command_within_bundle!('rails g rspec:install')
       remove_from_file '.rspec', '--warnings'
     end
