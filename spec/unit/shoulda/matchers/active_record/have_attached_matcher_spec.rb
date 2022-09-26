035822f4ec28d54e3398a2494cc44d3c401fb483
Pedro Paiva <pedro.v.s.paiva@gmail.com>
2022-09-26 07:22:03 -0300

    Install ActiveStorage migrations (#1458)

--- spec/unit/shoulda/matchers/active_record/have_attached_matcher_spec.rb
+++ spec/unit/shoulda/matchers/active_record/have_attached_matcher_spec.rb
@@ -1,35 +1,6 @@
 require 'unit_spec_helper'
 
 describe Shoulda::Matchers::ActiveRecord::HaveAttachedMatcher, type: :model do
-  before do
-    create_table :active_storage_blobs do |t|
-      t.string :key, null: false
-      t.string :filename, null: false
-      t.string :content_type
-      t.text :metadata
-      t.bigint :byte_size, null: false
-      t.string :checksum, null: false
-      t.datetime :created_at, null: false
-
-      t.index [:key], unique: true
-    end
-
-    create_table :active_storage_attachments do |t|
-      t.string :name, null: false
-      t.references :record, null: false, polymorphic: true, index: false
-      t.references :blob, null: false
-
-      t.datetime :created_at, null: false
-
-      t.index [:record_type, :record_id, :name, :blob_id],
-        name: 'index_active_storage_attachments_uniqueness', unique: true
-
-      # The original rails migration has a foreign key.
-      # Since this messes up the clearing of the database, it's removed here.
-      # t.foreign_key :active_storage_blobs, column: :blob_id
-    end
-  end
-
   describe 'have_one_attached' do
     describe '#description' do
       it 'returns the message with the name of the association' do
