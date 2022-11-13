bae538145ddfd9f66f302cd5e5b23f90320fceb9
Dorian <dorian@kundigo.pro>
2022-11-13 13:31:34 +0100

    Update validate_absence_of_matcher.rb
    Remove trailing whitespace

--- lib/shoulda/matchers/active_model/validate_absence_of_matcher.rb
+++ lib/shoulda/matchers/active_model/validate_absence_of_matcher.rb
@@ -118,7 +118,7 @@ module Shoulda
             when :datetime, :time, :timestamp then Time.current
             when :date then Date.new
             when :binary then '0'
-            when :uuid then SecureRandom.uuid  
+            when :uuid then SecureRandom.uuid
             else 'an arbitrary value'
             end
           end
