b2815bab0b9da4ad64e22f9decb3c5d977dd3d86
Jose Carlos Arenas <jarenas9539@gmail.com>
2023-06-09 21:25:02 +0100

    Adding validations for as_array option for ActiveModel attribute

--- spec/unit/shoulda/matchers/active_model/validate_length_of_matcher_spec.rb
+++ spec/unit/shoulda/matchers/active_model/validate_length_of_matcher_spec.rb
@@ -554,6 +554,97 @@ but this could not be proved.
     end
   end
 
+  context 'whith ActiveModel attribute marked as array' do
+    context 'an attribute with a non-zero minimum length validation' do
+      it 'accepts ensuring the correct minimum length' do
+        expect(define_active_model_validating_length(minimum: 4)).
+          to validate_length_of(:attr).as_array.is_at_least(4)
+      end
+
+      it 'rejects ensuring a lower minimum length with any message' do
+        expect(define_active_model_validating_length(minimum: 4)).
+          not_to validate_length_of(:attr).as_array.is_at_least(3).with_short_message(/.*/)
+      end
+
+      it 'rejects ensuring a higher minimum length with any message' do
+        expect(define_active_model_validating_length(minimum: 4)).
+          not_to validate_length_of(:attr).as_array.is_at_least(5).with_short_message(/.*/)
+      end
+
+      it 'does not override the default message with a blank' do
+        expect(define_active_model_validating_length(minimum: 4)).
+          to validate_length_of(:attr).as_array.is_at_least(4).with_short_message(nil)
+      end
+
+      it 'fails when used in the negative' do
+        assertion = lambda do
+          expect(define_active_model_validating_length(minimum: 4)).
+            not_to validate_length_of(:attr).as_array.is_at_least(4)
+        end
+
+        message = <<-MESSAGE
+Expected Example not to validate that the length of :attr is at least 4,
+but this could not be proved.
+  After setting :attr to ‹["x", "x", "x", "x"]›, the matcher expected
+  the Example to be invalid, but it was valid instead.
+        MESSAGE
+
+        expect(&assertion).to fail_with_message(message)
+      end
+    end
+
+    context 'an attribute with a minimum length validation of 0' do
+      it 'accepts ensuring the correct minimum length' do
+        expect(define_active_model_validating_length(minimum: 0)).
+          to validate_length_of(:attr).as_array.is_at_least(0)
+      end
+    end
+
+    context 'an attribute with a maximum length' do
+      it 'accepts ensuring the correct maximum length' do
+        expect(define_active_model_validating_length(maximum: 4)).
+          to validate_length_of(:attr).as_array.is_at_most(4)
+      end
+
+      it 'rejects ensuring a lower maximum length with any message' do
+        expect(define_active_model_validating_length(maximum: 4)).
+          not_to validate_length_of(:attr).as_array.is_at_most(3).with_long_message(/.*/)
+      end
+
+      it 'rejects ensuring a higher maximum length with any message' do
+        expect(define_active_model_validating_length(maximum: 4)).
+          not_to validate_length_of(:attr).as_array.is_at_most(5).with_long_message(/.*/)
+      end
+
+      it 'does not override the default message with a blank' do
+        expect(define_active_model_validating_length(maximum: 4)).
+          to validate_length_of(:attr).as_array.is_at_most(4).with_long_message(nil)
+      end
+    end
+
+    context 'an attribute with a required exact length' do
+      it 'accepts ensuring the correct length' do
+        expect(define_active_model_validating_length(is: 4)).
+          to validate_length_of(:attr).as_array.is_equal_to(4)
+      end
+
+      it 'rejects ensuring a lower maximum length with any message' do
+        expect(define_active_model_validating_length(is: 4)).
+          not_to validate_length_of(:attr).as_array.is_equal_to(3).with_message(/.*/)
+      end
+
+      it 'rejects ensuring a higher maximum length with any message' do
+        expect(define_active_model_validating_length(is: 4)).
+          not_to validate_length_of(:attr).as_array.is_equal_to(5).with_message(/.*/)
+      end
+
+      it 'does not override the default message with a blank' do
+        expect(define_active_model_validating_length(is: 4)).
+          to validate_length_of(:attr).as_array.is_equal_to(4).with_message(nil)
+      end
+    end
+  end
+
   def define_model_validating_length(options = {})
     options = options.dup
     attribute_name = options.delete(:attribute_name) { :attr }
@@ -563,6 +654,15 @@ but this could not be proved.
     define_model(:example, attribute_name => { type: type, options: { array: array } }) do |model|
       model.validates_length_of(attribute_name, options)
     end
+  end
+
+  def define_active_model_validating_length(options)
+    attribute_name = options.delete(:attribute_name) { :attr }
+
+    define_active_model_class('Example', accessors: []) do
+      attribute attribute_name, array: true
+      validates_length_of(attribute_name, options)
+    end.new
   end
 
   def validating_array_length(options = {})
