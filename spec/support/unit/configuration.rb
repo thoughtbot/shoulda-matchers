e11a42152ceac5d9d4ad6350713e968c5f190f1c
Matheus Sales <matheus_usales@hotmail.com>
2023-12-11 17:30:46 -0300

    refactor: Enhance unit test helpers loading
    This commit refactors the way we load unit test helpers. Instead of
    requiring each file individually, we now have a single file that
    requires all of them. This makes it easier to add new helpers and
    ensures that they are loaded in the correct order.

---
+++ spec/support/unit/configuration.rb
@@ -0,0 +1,26 @@
+module UnitTests
+  class Configuration
+    CLASSES = %i[
+      ActiveModelHelpers
+      ActiveModelVersions
+      ActiveRecordVersions
+      ClassBuilder
+      ColumnTypeHelpers
+      ControllerBuilder
+      DatabaseHelpers
+      I18nFaker
+      MailerBuilder
+      MessageHelpers
+      ModelBuilder
+      RailsVersions
+      ValidationMatcherScenarioHelpers
+    ].freeze
+
+    def self.configure_example_groups(config)
+      CLASSES.each do |class_name|
+        constantized_class = "UnitTests::#{class_name}"
+        Object.const_get(constantized_class).configure_example_group(config)
+      end
+    end
+  end
+end
