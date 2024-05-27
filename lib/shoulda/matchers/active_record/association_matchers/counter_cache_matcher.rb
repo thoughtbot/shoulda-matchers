d5f32ee57249e22bce125af4a24e2b1050836dcd
Theodor Tonum <theodor@tonum.no>
2024-05-27 20:43:21 +0200

    Support Rails 8.0 counter_cache configuration
    See https://github.com/rails/rails/pull/51453

--- lib/shoulda/matchers/active_record/association_matchers/counter_cache_matcher.rb
+++ lib/shoulda/matchers/active_record/association_matchers/counter_cache_matcher.rb
@@ -19,10 +19,7 @@ module Shoulda
           def matches?(subject)
             self.subject = ModelReflector.new(subject, name)
 
-            if option_verifier.correct_for_string?(
-              :counter_cache,
-              counter_cache,
-            )
+            if correct_value?
               true
             else
               self.missing_option = "#{name} should have #{description}"
@@ -34,8 +31,41 @@ module Shoulda
 
           attr_accessor :subject, :counter_cache, :name
 
+          def correct_value?
+            expected = normalize_value
+
+            if expected.is_a?(Hash)
+              option_verifier.correct_for_hash?(
+                :counter_cache,
+                expected,
+              )
+            else
+              option_verifier.correct_for_string?(
+                :counter_cache,
+                expected,
+              )
+            end
+          end
+
           def option_verifier
             @_option_verifier ||= OptionVerifier.new(subject)
+          end
+
+          def normalize_value
+            if Rails::VERSION::MAJOR >= 8
+              case counter_cache
+              when true
+                { active: true, column: nil }
+              when String, Symbol
+                { active: true, column: counter_cache.to_s }
+              when Hash
+                ({ active: true }).merge(counter_cache)
+              else
+                raise ArgumentError, 'Invalid counter_cache option'
+              end
+            else
+              counter_cache
+            end
           end
         end
       end
