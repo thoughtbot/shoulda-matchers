092747bd1d00d518c71a9af987ea2a13f9d977e5
Matheus Sales <matheus_usales@hotmail.com>
2026-02-01 09:42:24 -0300

    fix: Prevent ActiveRecord constant leak in uniqueness matcher (#1694)
    When using the validate_uniqueness_of matcher, test model classes were
    being added to ActiveRecord::Base.descendants and not being removed
    after the matcher ran. This could lead to unexpected behavior in tests
    that rely on the list of ActiveRecord descendants.
    
    Closes #1663

--- lib/shoulda/matchers/active_record/uniqueness/namespace.rb
+++ lib/shoulda/matchers/active_record/uniqueness/namespace.rb
@@ -10,7 +10,7 @@ module Shoulda
           end
 
           def has?(name)
-            constant.const_defined?(name)
+            constant.const_defined?(name, false)
           end
 
           def set(name, value)
@@ -18,8 +18,25 @@ module Shoulda
           end
 
           def clear
+            test_model_classes = []
+
             constant.constants.each do |child_constant|
-              constant.__send__(:remove_const, child_constant)
+              child = constant.const_get(child_constant)
+
+              if defined?(::ActiveRecord::Base) &&
+                 child.is_a?(Class) &&
+                 child < ::ActiveRecord::Base
+                test_model_classes << child
+              end
+
+              constant.remove_const(child_constant)
+            rescue NameError
+              # Constant may have been removed elsewhere; ignore
+            end
+
+            if defined?(::ActiveSupport::DescendantsTracker) &&
+               test_model_classes.any?
+              ::ActiveSupport::DescendantsTracker.clear(test_model_classes)
             end
           end
 
